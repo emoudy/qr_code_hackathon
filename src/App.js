810069496a813fb13897dc5924a987777e6ba2f3
Elda Moudy <elda@moudy.net>
2022-09-13 22:31:08 -0700

    fields

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello Hackathon
-        </a>
-      </header>
-    </div>
+const App = () => {
+    return (
+        <div>
+            <form className='form'>
+                <div className='form-control'>
+                    <label htmlFor='name'>Name: </label>
+                    <input
+                        type='text'
+                        id='name'
+                        name='name'
+                    />
+                    <label htmlFor='email'>Name: </label>
+                    <input
+                        type='text'
+                        id='email'
+                        name='email'
+                    />
+                    <label htmlFor='phone'>Name: </label>
+                    <input
+                        type='text'
+                        id='phone'
+                        name='phone'
+                    />
+                </div>
+                <button type='submit' onSubmit={console.log("Submitting!")}>Register</button>
+            </form>
+        </div>
   );
-}
+};
 
 export default App;
