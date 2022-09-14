e942c6b945bc9fe97b5ef0fa38388d785554a56e
Elda Moudy <elda@moudy.net>
2022-09-13 23:28:45 -0700

    name

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React,  {useState} from 'react';
 
 const App = () => {
+  const [name, setName] = useState('');
+
+  const handleSubmit = event => {
+    console.log('handleSubmit ran');
+    event.preventDefault();
+    console.log("name", name)
+    setName('');
+  }
+  
     return (
         <div>
             <form className='form'>
@@ -10,6 +19,8 @@ const App = () => {
                         type='text'
                         id='name'
                         name='name'
+                        onChange={event => setName(event.target.value)}
+                        value={name}
                     />
                     <label htmlFor='email'>Email: </label>
                     <input
@@ -24,7 +35,7 @@ const App = () => {
                         name='phone'
                     />
                 </div>
-                <button type='submit' onSubmit={console.log("Submitting!")}>Add person</button>
+                <button type='submit' onSubmit={handleSubmit}>Add person</button>
             </form>
         </div>
   );
