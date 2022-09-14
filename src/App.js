56abf0d5d4cfad013a73592edd6ae921a12ceb2e
Elda Moudy <elda@moudy.net>
2022-09-13 22:54:40 -0700

    reverting

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Hackathon
+          Learn React
         </a>
       </header>
     </div>
