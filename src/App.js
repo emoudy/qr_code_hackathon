9e45342c36d90e412fea8de4e054f4b94a220cdc
Elda Moudy <elda@moudy.net>
2022-09-13 23:21:31 -0700

    Submitting

--- src/App.js
+++ src/App.js
@@ -5,14 +5,26 @@ const App = () => {
         <div>
             <form className='form'>
                 <div className='form-control'>
-                    <label htmlFor='firstName'>Name: </label>
+                    <label htmlFor='name'>Name: </label>
                     <input
                         type='text'
-                        id='firstName'
-                        name='firstName'
+                        id='name'
+                        name='name'
+                    />
+                    <label htmlFor='email'>Email: </label>
+                    <input
+                        type='text'
+                        id='email'
+                        name='email'
+                    />
+                    <label htmlFor='phone'>Phone: </label>
+                    <input
+                        type='text'
+                        id='phone'
+                        name='phone'
                     />
                 </div>
-                <button type='submit'>Add person</button>
+                <button type='submit' onSubmit={console.log("Submitting!")}>Add person</button>
             </form>
         </div>
   );
